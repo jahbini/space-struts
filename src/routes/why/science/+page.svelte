8190035eba04ada01a48dada9bbf5bd64a98d72e
James Hinds <jahbini@icloud.com>
2023-07-02 08:33:27 -0400

    add why/science subpage

---
+++ src/routes/why/science/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+/*  /src/routes/(front)/how/template */
+import Card from '/src/lib/CardLeftImage.svelte';
+</script>
+
+<template lang="pug" >
+<Card image="/images/space-struts/IMG_2545.jpeg" heading="Mathematics" >
+
+  h3 So, What science?
+  h6 Physics, Chemistry, Let me count the ways...
+  ol
+    li 
+      a(href="https://phys.org/news/2017-07-tetrahedron.html") Measuring the Earth's motion with a Tetrahedron
+    li 
+      a(href="https://en.wikipedia.org/wiki/Capsid") The exact shape of your friendly neighborhood virus.
+    li 
+      a(href="https://www.nature.com/articles/s41596-020-0355-z") It's in your DNA
+    li 
+      a(href="https://www.sciencedirect.com/science/article/abs/pii/S1385894722013134") Octahedrons fix cancer???
+    li 
+      a(href="https://www.cosmic-core.org/free/article-170-genetics-the-geometry-of-dna-part-3-dr-mark-white/") Dodecahedrons involved in DNA stacking. Stacking DNA: is that legal?
+    li 
+      a(href="https://www.forbes.com/sites/kevinknudson/2015/07/23/the-mathematics-of-buckyballs/?sh=774205616f77") Buckyballs: The good kind.
+    li 
+      a(href="https://hjcowdery.com/diatoms") Microscopic Diatoms made Alfred Nobel RICH!!!
+    li 
+      a(href="https://www.facebook.com/watch/?v=353387425202178") Dodecahedron Robots work underwater.
+  p
+    | Honestly, do you really need more?
+
+  h5 Never borrower or lender, Always teacher and student
+  h6 -- The Celarien.
+
+</Card>
+</template>
+
+<style>
+
+</style>
