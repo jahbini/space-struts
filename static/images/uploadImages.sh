dc8940cc5785941e973753edc01c520049c5d4a4
James Hinds <jahbini@icloud.com>
2023-08-21 09:52:40 -0400

    lock in the image upload directory

--- static/images/uploadImages.sh
+++ static/images/uploadImages.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-rsync -v -r --progress ./space-struts/test.jpg  mar1on@spacestruts.com:~/IOTstack/volumes/space-struts/space-struts
+rsync -v -r --progress ./space-struts/  mar1on@spacestruts.com:~/IOTstack/volumes/space-struts/space-struts
