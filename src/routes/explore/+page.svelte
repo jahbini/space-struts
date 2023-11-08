8df62d549525e24e16f5d9ef3004c59cd25bc6f9
James Hinds <jahbini@icloud.com>
2023-11-08 12:38:39 -0500

    intro talky talky for vertex-city and fire-jim OK

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,10 +1,33 @@
 <script>
   ///Users/jahbini/development/ucoa/space-struts/src/routes/(front)/how
+  import StepWizard from 'svelte-step-wizard'
 </script>
 
-<template lang="pug">
 
-h2 You are here at 
-  em how.
+<h3> You have reached the gateway to <a href="./explore/vertex-city">Vertex City</a>, a prototype of the future.</h3>
 
-</template>
+<p>The population of vertex city lives at the points of the Platonic solids <a href="https://en.wikipedia.org/wiki/Dodecahedron">ref#wikipedia</a></p>
+<ol>
+<li>
+Step 1: Select one or more of the Solids
+</li>
+<li>
+Step 2: Add some line segments by length
+</li>
+<li>
+Step 3: remove the vertices or labels if you want
+</li>
+<li>
+Step 4: Gaze at the left and right views of your vertex city.  3D viewing is easy for some, hard for some: don’t hurt yourself.
+</li>
+<li>
+Step 5: rotate the left view to get the full 3D effects.
+</li>
+<li>
+Step 6: If you want to save your images, just click the links under each view: you will get an exact SVG image of your city.
+</li>
+
+<li>
+Step 7: Sign the <a href="./explore/fire-jim">“Fire Jim”</a> petition (King D. made me put that in)
+</ol>
+
---
+++ src/routes/explore/fire-jim/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  ///Users/jahbini/development/ucoa/space-struts/src/routes/(front)/how
+</script>
+
+
+<h3>Geemo Beamo sez: A vision </h3>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/G29XfHXXnEQ?si=j0__tnAAVcnMtfhL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+<h3>King D sez: A Prophesy </h3>
+
+<p>Fire this guy!  Only YOU are competent enough to fulfull this Vision that I, King D, planted in the brain of the gullible Geemo Beamo.
+</p>
+<p>Fortunately, that will be easy.  Just give him any reason to sign this whole shebang over YOUR competent hands.
+
+That's why there is a Contact button on the page.  Use it.
+</p>
+<p>
+Respectfully,
+The Honorable and Most Majestic
+</p>
+<h4>King D</h4>
+
+
