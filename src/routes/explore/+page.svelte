a3976003c005e57dd48fcc58bdb19480ffd15fe6
James Hinds <jahbini@icloud.com>
2023-11-23 16:10:18 -0500

    remove link referenc to vertex-city

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 
-<h3> You have reached the gateway to <a href="./explore/vertex-city">Vertex City</a>, a prototype of the future.</h3>
+<h3> You have reached the gateway to <a href="./explore/triangle-city">Triangle City</a>, a prototype of the future.</h3>
 
 <p>The population of vertex city lives at the points of the Platonic solids <a href="https://en.wikipedia.org/wiki/Dodecahedron">ref#wikipedia</a></p>
 <ol>
@@ -12,13 +12,13 @@
 Step 1: Select one or more of the Solids
 </li>
 <li>
-Step 2: Add some line segments by length
+Step 2: Add some line segments by length and select triangles by angles from them.
 </li>
 <li>
 Step 3: remove the vertices or labels if you want
 </li>
 <li>
-Step 4: Gaze at the left and right views of your vertex city.  3D viewing is easy for some, hard for some: don’t hurt yourself.
+Step 4: Gaze at the left and right views of your triangle city.  3D viewing is easy for some, hard for some: don’t hurt yourself.
 </li>
 <li>
 Step 5: rotate the left view to get the full 3D effects.
