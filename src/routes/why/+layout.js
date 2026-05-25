454da7b39256b804195cc0517530a6cea2e913db
Jim Hinds <jahbini@icloud.com>
2026-05-25 19:22:26 +1000

    why math back on

--- src/routes/why/+layout.js
+++ src/routes/why/+layout.js
@@ -1,4 +1,4 @@
 /** {import('./$types').LayoutData}  */
 export function load() {
-  return { navPages: import.meta.glob("./*/+page.svelte"), navRoute: "why" };
+  return { navPages: import.meta.glob("./*/+page.sv*"), navRoute: "why" };
 }
